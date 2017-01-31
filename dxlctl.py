5d0549e413ff74354358a7cecb940a163437e2e2
Alex Ray <a@machinaut.com>
2017-01-30 19:25:10 -0800

    now kinda reliably resets to resting position

--- dxlctl.py
+++ dxlctl.py
@@ -9,48 +9,44 @@ if len(sys.argv) >= 2:
     port = sys.argv[1]
 
 # Connect to serial device at 115200 baud (matches arduino speed)
-ser = serial.Serial(port, 115200, timeout=0.2)
+ser = serial.Serial(port, 115200, timeout=1.0)
 ser.read(1000)  # empty read buffer
 ser.flush()
 
 print('disable all the actuators')
 for i in range(1, 7):
     ser.write('Cs{},24,0\n'.format(i).encode())  # Torque Disable
-    ser.write('Cg{},24\n'.format(i).encode())  # Torque Disable
-    print(ser.read(1000))
     time.sleep(0.3)
+    ser.flush()
+
 print('set the actuator torque limits')
 for i in range(1, 7):
     ser.write('CS{},34,1023\n'.format(i).encode())  # Torque Limit 50%
-    ser.write('CG{},34\n'.format(i).encode())  # Torque Limit 50%
-    print(ser.read(1000))
     time.sleep(0.3)
+    ser.flush()
+
 print('enable all the actuators')
 for i in range(1, 7):
     ser.write('Cs{},24,1\n'.format(i).encode())  # Torque Enable
-    ser.write('Cg{},24\n'.format(i).encode())  # Torque Enable
-    print(ser.read(1000))
     time.sleep(0.3)
+    ser.flush()
+
 print('go to resting position')
 rest = [2048, 1000, 1100, 1700, 512, 512]
 for i, val in enumerate(rest):
     ser.write('CS{},30,{}\n'.format(i+1, val).encode())  # Goal position
-    ser.write('CG{},30\n'.format(i+1).encode())  # Goal position
-    print(ser.read(1000))
     time.sleep(0.3)
+    ser.flush()
+
 print('disable all the actuators')
 for i in range(1, 7):
     ser.write('Cs{},24,0\n'.format(i).encode())  # Torque Disable
-    ser.write('Cg{},24\n'.format(i).encode())
-    print(ser.read(1000))
     time.sleep(0.3)
-print('read alarm status')
+    ser.flush()
+
+ser.read(1000) # empty read buffer
+print('read present position')
 for i in range(1, 7):
-    ser.write('Cg{},18\n'.format(i).encode())  # Torque Enable
-    print(ser.readline())
-    time.sleep(0.3)
-print('read torque limit')
-for i in range(1, 7):
-    ser.write('CG{},34\n'.format(i).encode())  # Torque Limit
+    ser.write('CG{},36\n'.format(i).encode())  # Torque Limit
     print(ser.readline())
     time.sleep(0.3)
